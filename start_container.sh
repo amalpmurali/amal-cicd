7b1b5a4b0901ec12c7d4a65c301f7b3df1a61da0
amalpmurali <amalpmurali4@gmail.com>
2024-09-21 02:04:38 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull amludevops/cicd-demo
+docker pull amludevops/cicd-demo
 
 # Run the Docker image as a container
 docker run -dp 5000:5000 amludevops/cicd-demo
